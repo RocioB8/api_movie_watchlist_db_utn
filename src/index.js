454b679a65842fe89892ba8577afdb3d9352b48d
rociobe <rociobe08@gmail.com>
2026-03-21 17:55:41 -0300

    fix conexion mongodb y config server

--- src/index.js
+++ src/index.js
@@ -8,26 +8,31 @@ import { authRouter } from "./routes/authRouter.js"
 import { movieRouter } from "./routes/movieRouter.js"
 import { authMiddleware } from "./middleware/authMiddleware.js"
 
-
 const serverHttp = express()
 
 serverHttp.use(cors())
 serverHttp.use(express.json())
 
 serverHttp.use("/api/movies", authMiddleware, movieRouter)
-
 serverHttp.use("/api/auth", authRouter)
 
-
-
 serverHttp.use((req, res) => {
-  res.status(404).json({ success: false, error: "Ruta no válida o inexistente<" })
+  res.status(404).json({ success: false, error: "Ruta no válida o inexistente" })
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
-serverHttp.listen(PORT, () => {
-  console.log(`✅ Servidor http en escucha para el puerto http://127.0.0.1:${PORT}`)
+const startServer = async () => {
+  try {
+    await conectDb()
 
-  conectDb()
-})
+    serverHttp.listen(PORT, () => {
+      console.log(`✅ Servidor corriendo en puerto ${PORT}`)
+    })
+
+  } catch (error) {
+    console.error("❌ Error al iniciar el servidor:", error)
+  }
+}
+
+startServer()
